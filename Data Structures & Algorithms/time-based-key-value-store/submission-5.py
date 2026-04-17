aad1171d8e3ddcca0b1a05f0c8081994236c283c
AnyaH0831 <141371192+AnyaH0831@users.noreply.github.com>
2026-04-16 21:50:11 -0400

    Add: time-based-key-value-store - submission-5

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-5.py
@@ -0,0 +1,28 @@
+from collections import defaultdict
+from sortedcontainers import SortedDict
+
+class TimeMap:
+    
+    def __init__(self):
+        self.timeMap = defaultdict(SortedDict)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.timeMap[key][timestamp] = value
+        # print(self.timeMap)
+
+    def get(self, key: str, timestamp: int) -> str:
+        
+        if key not in self.timeMap:
+            return ""
+
+        timestamps = self.timeMap[key]
+        # key -- timestamp; value -- value
+
+        idx = timestamps.bisect_right(timestamp) - 1
+
+        if idx >= 0:
+            closest_time = timestamps.iloc[idx]
+            return timestamps[closest_time]
+        return ""
+
+
